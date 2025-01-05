db5667c9da38b4d2656a6fe676ab97bd5f956536
puspitasahu <50769308+puspitasahu@users.noreply.github.com>
2025-01-05 16:33:13 +0530

    Update variables.tf

--- ec2-instance/variables.tf
+++ ec2-instance/variables.tf
@@ -1,1 +1,12 @@
 
+variable "ami" {
+  description = "AMI for the instance"
+}
+
+variable "instance_type" {
+  description = "Instance type"
+}
+
+variable "instance_name" {
+  description = "Name of the EC2 instance"
+}
