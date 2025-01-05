5d5e1f5b064894a3f8e7d1a97cc29dec7c14fd52
puspitasahu <50769308+puspitasahu@users.noreply.github.com>
2025-01-05 15:42:04 +0530

    Create variables.tf
    creating variable  terraform

---
+++ variables.tf
@@ -0,0 +1,14 @@
+variable "aws_region"{
+    description ="AWS Region"
+    default = "us-east-1"
+}
+
+variable "aws_type"{
+    description ="AWS Region"
+    default = "t3.medium"
+}
+
+variable "aws_ami"{
+    description ="AWS ami"
+    default = "ami-01816d07b1128cd2d"
+}
