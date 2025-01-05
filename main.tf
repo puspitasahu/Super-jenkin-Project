58d5e3688a2ed8c314128196188412a23aff77f5
puspitasahu <50769308+puspitasahu@users.noreply.github.com>
2025-01-05 16:00:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,33 @@
 
+terraform{
+    required_providers{
+        aws ={
+            source="hashicorp/aws"
+            version ="~>5.0"
+        }        
+        
+    }
+}
+
+provider "aws"{
+    region = var.aws_region
+}
+
+module "nexus_instance" {
+    source="./ec2_instance"
+    ami=var.aws_ami
+    instance_type=var.aws_type
+    instance_name="nexus"
+}
+module "sonar_instance" {
+    source="./ec2_instance"
+    ami=var.aws_ami
+    instance_type=var.aws_type
+    instance_name="sonar"
+}
+module "test_instance" {
+    source="./ec2_instance"
+    ami=var.aws_ami
+    instance_type=var.aws_type
+    instance_name="test"
+}
